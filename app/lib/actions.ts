f33ba921d5cabb70bd5d3364bbcd95e8cb7298be
zhaoyao <edwardzhaoyao@gmail.com>
2026-02-25 14:36:33 +0800

    feat: Adding metadata

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -117,7 +117,7 @@ export async function deleteInvoice(id: string) {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
   } catch (error) {
     console.error(error);
-    return { message: "Database Error: Failed to Delete Invoice" };
+    // return { message: "Database Error: Failed to Delete Invoice" };
   }
   revalidatePath("/dashboard/invoices");
 }
